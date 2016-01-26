4a9c53013cf6d2c15e96f9eb4b2859adfe85f9f6
wangtao <764765614@qq.com>
2016-01-26 18:11:49 +0800

    Initial Commit

---
+++ 简单模仿网易新闻滚动条/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  简单模仿网易新闻滚动条
+//
+//  Created by 王涛 on 16/1/26.
+//  Copyright © 2016年 304. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
